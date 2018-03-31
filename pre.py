27de11c336dc3ba6df1826017fa8cff40ef87301
Mason Tian <masontian@nat-tvwna-inside-visitornet101-b-29148.princeton.org>
2018-03-31 13:00:20 -0400

    moved code into pre.py

--- pre.py
+++ pre.py
@@ -1,3 +1,4 @@
+# Handles initially processing the music
 from music21 import *
 import numpy as np
 import pandas as pd
@@ -120,6 +121,11 @@ def convert_notes_to_indexes(notes):
 
     return X, Y
 
+
+phrase_positions = []
+
+
+
 def split_into_phrases(phrase_positions, notesList):
     phrases = [notesList[x:y] for x, y in zip(phrase_positions, phrase_positions[1:])]
 
@@ -172,3 +178,31 @@ def sample():
   streams = converter.parse(filename)
   phraseStarts = parseStream(filename, streams)
   return on_off_representation(streams, phraseStarts)
+def on_off_representation(stream):
+
+    with open('indexes.csv', 'r', encoding='utf-8') as csv_file:
+        note_dict = dict(csv.reader(csv_file))
+
+    step = 0
+    current_notes = []
+    indices = []
+    data = []
+    for note in stream.notes:
+        thirty_two_length = int(note.quarterLength * 8)
+        if (thirty_two_length != 0):
+            current_notes.append(note)
+            for n in current_notes:
+                string_rep = str(n.pitch) + str(n.quarterLength)
+                indices.append([int(note_dict[string_rep]),step])
+                indices.append([int(note_dict[string_rep]),step + thirty_two_length - 1])
+                data += [1,1]
+            step += thirty_two_length
+            current_notes = []
+        else:
+            current_notes += note
+    print(indices)
+    print(data)
+    phrase = SparseTensor(indices, data, (len(note_dict), int(stream.duration.quarterLength * 8)))
+    return phrase
+
+
