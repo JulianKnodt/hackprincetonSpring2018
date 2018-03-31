61a9590d755c075a2175638ca57e538808f29f04
Mason Tian <masontian@nat-tvwna-inside-visitornet101-b-29148.princeton.org>
2018-03-31 00:41:37 -0400

    merged player and musicwriter

--- musicwriter.py
+++ musicwriter.py
@@ -2,6 +2,18 @@ from music21 import *
 import csv
 import sys
 import os
+from collections import defaultdict
+import csv
+
+# indexes should be a list of ints, the output from the neural network
+def convert_indexes_to_notes(indexes):
+    with open('indexes.csv', 'r', encoding='utf-8') as csv_file:
+        note_dict = dict(csv.reader(csv_file))
+    
+    int_dict = dict((int, note) for note, int in note_dict.items())
+    for index in indexes:
+        print(int_dict[str(index)])
+
 
 orig_stdout = sys.stdout
 f = open('GoldbergVariationsRawData.csv', 'w')
