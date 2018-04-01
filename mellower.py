d14590746894ba84c6248881ecb071da19a1440b
Nick Kim <nk6@princeton.edu>
2018-04-01 07:07:49 -0400

    Add files via upload

--- mellower.py
+++ mellower.py
@@ -2,56 +2,92 @@ from music21 import *
 
 #
 def mellower(notes):
-    print("MELLOWER: notes length " + str(len(notes)))
     returnNotes1 = []
     returnNotes2 = []
     notesNext = []
     counter = 0
     previousNote = note.Note('c8')
+    runner = True
     for i in notes:
-        if (i != notes[0]):
-            notesNext.append(i)
+        if (runner):
+            runner = False
+            continue
+        notesNext.append(i)
     for thisNote in notes:
-        avg = 0
-        if (counter == 0):
-            avg = (thisNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum) / 2
-        elif (counter < len(notes) - 1):
-            avg = (previousNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 3
-        if (counter == len(notes) - 1):
-            avg = (previousNote.pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 2
-        returnNotes1.append(thisNote.transpose(interval.GenericInterval(int(avg) - thisNote.pitch.diatonicNoteNum)))
-        counter = counter + 1
-        previousNote = thisNote
+        if(str(type(thisNote)) == str("<class 'music21.note.Rest'>")):
+            returnNotes1.append(thisNote)
+            counter = counter + 1
+        else:
+            avg = 0
+            if (counter == 0):
+                if (str(type(notesNext[counter])) == str("<class 'music21.note.Rest'>")):
+                    avg = thisNote.pitch.diatonicNoteNum
+                else:
+                    avg = (thisNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum) / 2
+            elif (counter < len(notes) - 1):
+                print("COUNTER IS " + str(counter))
+                if (str(type(notesNext[counter])) == str("<class 'music21.note.Rest'>")):
+                    avg = (previousNote.pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 2
+                else:
+                    avg = (previousNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 3
+            if (counter >= len(notes) - 1):
+                avg = (previousNote.pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 2
+            if (int(avg) - thisNote.pitch.diatonicNoteNum != 0):
+                returnNotes1.append(thisNote.transpose(interval.GenericInterval(int(avg) - thisNote.pitch.diatonicNoteNum)))
+            else:
+                returnNotes1.append(thisNote)
+            counter = counter + 1
+            previousNote = thisNote
     counter = 0
+    print("First size " + str(len(returnNotes1)))
+
     for thisNote in reversed(returnNotes1):
-        avg = 0
-        if (counter == 0):
-            avg = (thisNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum) / 2
-        if (counter < len(notes) - 1):
-            avg = (previousNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 3
-        if (counter == len(notes) - 1):
-            avg = (previousNote.pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 2
-        returnNotes2.append(thisNote.transpose(interval.GenericInterval(int(avg) - thisNote.pitch.diatonicNoteNum)))
-        counter = counter + 1
-        previousNote = thisNote
+        if(str(type(thisNote)) == str("<class 'music21.note.Rest'>")):
+            returnNotes2.append(thisNote)
+            counter = counter + 1
+        else:
+            avg = 0
+            if (counter == 0):
+                if (str(type(notesNext[counter])) == str("<class 'music21.note.Rest'>")):
+                    avg = thisNote.pitch.diatonicNoteNum
+                else:
+                    avg = (thisNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum) / 2
+            elif (counter < len(notes) - 1):
+                if (str(type(notesNext[counter])) == str("<class 'music21.note.Rest'>")):
+                    avg = (previousNote.pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 2
+                else:
+                    avg = (previousNote.pitch.diatonicNoteNum + notesNext[counter].pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 3
+            if (counter >= len(notes) - 1):
+                avg = (previousNote.pitch.diatonicNoteNum + thisNote.pitch.diatonicNoteNum) / 2
+            if (int(avg) - thisNote.pitch.diatonicNoteNum != 0):
+                returnNotes2.append(thisNote.transpose(interval.GenericInterval(int(avg) - thisNote.pitch.diatonicNoteNum)))
+            else:
+                returnNotes2.append(thisNote)
+            counter = counter + 1
+            previousNote = thisNote
+
+
     return reversed(returnNotes2)
+#
+# n1 = note.Rest()
+# n2 = note.Note('e3')
+# n3 = note.Rest()
+# n4 = note.Note('g2')
+# n5 = note.Note('b4')
+# n6 = note.Note('e3')
+# n7 = note.Rest()
+#
+# noteos = [n1, n2, n3, n4, n5, n6, n7]
+# for i in noteos:
+#     if(str(type(i)) != str("<class 'music21.note.Rest'>")):
+#         print(i.pitch.diatonicNoteNum)
+# new = mellower(noteos)
+# for i in new:
+#     if(str(type(i)) != str("<class 'music21.note.Rest'>")):
+#         print(i.pitch.diatonicNoteNum)
 
-n1 = note.Note("c2")
-n2 = note.Note('e3')
-n3 = note.Note('f3')
-n4 = note.Note('g2')
-n5 = note.Note('b4')
-n6 = note.Note('b5')
-n7 = note.Note('c4')
 
-noteos = [n1, n2, n3, n4, n5, n6, n7]
-print (noteos)
-for i in noteos:
-    print(i.pitch.diatonicNoteNum)
-new = mellower(noteos)
-print (new)
-for i in new:
-    print(i.pitch.diatonicNoteNum)
+
 
 # c4 = note.Note("c4")
 # print(c4.pitch.diatonicNoteNum)
