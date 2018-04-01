81143e14201478b8696cb8e835d68af2cbd178fb
julianknodt <julianknodt@gmail.com>
2018-03-31 22:01:00 -0400

    Created Helper for processing phrases
    Helper can easily process phrases

--- classifications.py
+++ classifications.py
@@ -1,3 +1,4 @@
+import urllib.request as req
 import numpy as np
 import music21 as m21
 import math
@@ -10,7 +11,7 @@ def classify_phrase(phrase):
   mean_value = 0
   for note in phrase:
     length += note.duration.quarterLength/4
-    if isinstance(note, m21.note.Rest):
+    if not(isinstance(note, m21.note.Note)):
       continue
     minV = min(note.pitch.frequency, minV)
     maxV = max(note.pitch.frequency, maxV)
@@ -38,35 +39,46 @@ def is_deccelerating(phrase):
       return False
   return True
 
-def summary_statistics(raw_notes, classification_states):
+def phrases(raw_notes, classification_states):
   current_phrase = []
-  result = np.array([])
+  result = []
   i = 0
   for part in raw_notes:
     for note in part.notesAndRests.stream():
       i += 1
       if i in classification_states:
-        result = np.append(result, current_phrase)
+        result += [current_phrase]
         current_phrase = []
       else:
         current_phrase += [note]
 
   if len(current_phrase) != 0:
-    result = np.append(result, current_phrase)
+    result += [current_phrase]
   return result
+
+def generate_csv(midi_path):
+  result = []
+  raw_notes = m21.converter.parse(midi_path)
+  for part in raw_notes:
+    for note in part.notesAndRests.stream():
+      if isinstance(note, m21.note.Note):
+        result += [f"{note.pitch.name}, {note.pitch.octave}, {note.duration.quarterLength/4}"]
+      else:
+        result += [f"R, 0, {note.duration.quarterLength/4}"]
+  return '\n'.join(result)
+
+url = 'http://www.bachcentral.com/sinfon'
+#for i in range(1, 15):
+#  print(generate_csv(url + f'/sinfon{i}.mid'))
 
 
 classification_functions = [is_accelerating, is_deccelerating]
 def concat_phrases(phrases):
-  result = []
-  for phrase in phrases:
-    data = {
+  return map(lambda phrase: {
       "summary": classify_phrase(phrase),
       "classifications": map(lambda c: c(phrase), classification_functions),
       "phrase": phrase,
-    }
-    result += [data]
-  return result
+    }, phrases)
 
 # TEST:
-print(summary_statistics(m21.converter.parse('988-v01.mid'), [10]))
+print(list(concat_phrases(phrases(m21.converter.parse('http://www.bachcentral.com/WTCBkI/Fugue1.mid'), [10])))[0])
